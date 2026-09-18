bbcac2631fe2d5744b7c1e01aaccf1fd293eb6cf
Kevin426430 <cherrysavoryy@gmail.com>
2026-09-18 09:38:53 +0000

    commit10

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
 
+int main()
+{
+    int n = 8;
+    int i;
+    int j;
+
+    for (i = 1; i <= n; i++)
+    {
+        // Afficher les espaces
+        for (j = 1; j <= n - i; j++)
+        {
+            printf(" ");
+        }
+
+        // Afficher les nombres croissants
+        for (j = 1; j <= i; j++)
+        {
+            printf("%d", j);
+        }
+
+        // Afficher les nombres décroissants
+        for (j = i - 1; j >= 1; j--)
+        {
+            printf("%d", j);
+        }
+
+        printf("\n");
+    }
+
+    printf("Generation de la pyramide terminee.\n");
+
+    return 0;
+}
